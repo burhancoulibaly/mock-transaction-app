14027b232b5629770934e761e1b3f6a404b787a5
burhancoulibaly <burhancoulibaly@gmail.com>
2020-07-18 00:51:35 -0400

    Delete environment.ts

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-   production: false,
-   MYSQL_PASSWORD: '@Burbur99',
-   ACCESS_TOKEN_SECRET: 'sdjkfnsjdnsdkfn',
-   REFRESH_TOKEN_SECRET: 'ijnweinwejijwej',
-   GRAPHQL_URI: 'http://localhost:3000/graphql'
-};
